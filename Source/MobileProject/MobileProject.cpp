31d16a4539a97389a6f3ebab41fffb22b2228e77
deulee <ideade9@gmail.com>
2025-08-04 18:15:57 +0900

    초기 프로젝트 세팅

---
+++ Source/MobileProject/MobileProject.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MobileProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MobileProject, "MobileProject" );
